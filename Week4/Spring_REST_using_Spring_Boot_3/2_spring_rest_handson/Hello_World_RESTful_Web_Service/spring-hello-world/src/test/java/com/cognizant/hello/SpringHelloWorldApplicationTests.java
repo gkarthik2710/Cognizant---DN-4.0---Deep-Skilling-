09d2380859161d31022b77f73592476c784ae6f3
gkarthik2710 <karthikkarthik47162@gmail.com>
2025-07-13 02:55:09 +0530

    Commit

---
+++ Week4/Spring_REST_using_Spring_Boot_3/2_spring_rest_handson/Hello_World_RESTful_Web_Service/spring-hello-world/src/test/java/com/cognizant/hello/SpringHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Week4/Spring_REST_using_Spring_Boot_3/2_spring_rest_handson/REST_Country_Web_Service/spring-country-rest/src/main/java/com/cognizant/springlearn/SpringCountryRestApplication.java
@@ -0,0 +1,11 @@
+package com.cognizant.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCountryRestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCountryRestApplication.class, args);
+	}
+}
